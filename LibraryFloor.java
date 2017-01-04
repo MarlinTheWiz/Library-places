09ec4c445e49e0b3a6c57f6a44054754410446b6
Marlin Jayasekera <marlinjayasekera@gmail.com>
2017-01-04 17:11:51 -0500

    Extend on a floor can be changed
    Decided that everything to do with floor changes should be in one class
    (LibraryFloor)

--- LibraryFloor.java
+++ LibraryFloor.java
@@ -1,8 +1,34 @@
 
 public class LibraryFloor {
 	
-	public LibraryFloor(){
-		
+	private static LibraryArea[] firstFloor; 
+	private static LibraryArea[] secondFloor;
+	private static LibraryArea[] thirdFloor; // Pretty sure the array lengths are wrong
+	private static LibraryArea[] fourthFloor;
+	private static LibraryArea[] fifthFloor;
+	
+	public LibraryFloor(LibraryArea[] floor, int num){
+		switch (num) {
+			
+		case 1:
+			firstFloor = floor;
+			break;
+		case 2:
+			secondFloor = floor;
+			break;
+		case 3:
+			thirdFloor = floor;
+			break;
+		case 4:
+			fourthFloor = floor;
+			break;
+		case 5:
+			fifthFloor = floor;
+			break;
+		default:
+			System.out.println("Enter a valid floor number");
+			
+		}
 	}
 	
 	
--- WebsterAreasLibrary.java
+++ WebsterAreasLibrary.java
@@ -1,11 +1,5 @@
 
 public class WebsterAreasLibrary {
-
-	private static LibraryArea[] firstFloor; 
-	private static LibraryArea[] secondFloor;
-	private static LibraryArea[] thirdFloor; // Pretty sure the array lengths are wrong
-	private static LibraryArea[] fourthFloor;
-	private static LibraryArea[] fifthFloor;
 	
 	public static void main(String[] args) {
 		
@@ -13,6 +7,8 @@ public class WebsterAreasLibrary {
 		// http://library.concordia.ca/locations/floor-plans.php?guid=web3
 		// Third floor Library areas
 		LibraryArea france = new LibraryArea("France", 3, 144);
+		LibraryArea india = new LibraryArea("India", 3, 144);
+		
 		
 		
 	}
